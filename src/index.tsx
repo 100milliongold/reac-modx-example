15301584191c98b0f27e6037a3df2d3cd73a1cf6
je.kim <gadian88@gmail.com>
2021-04-12 18:33:33 +0900

    modx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import TodoForm from "./component/TodoForm";
+import TodoList from "./component/TodoList";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoForm/>
+    <TodoList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
